2ece8c025f0cccb5eafda7fe18737ef8574a1f83
bbasung1 <bbasung1@daum.net>
2021-01-26 18:41:46 +0900

    need to debug

--- dupfilecheck.py
+++ dupfilecheck.py
@@ -10,5 +10,23 @@ for (path, dir, files) in os.walk("./"):
         data = f.read()
         md.append(hashlib.md5(data).hexdigest())
         nm.append(os.path.join(path, fn))
-for a in nm:
-    print(1)
+f = open("log.txt", "w")
+t=open("debug.txt","w")
+for a in md:
+    k = []
+    for i, name in enumerate(md):
+        if(a == name):
+            k.append(i)
+    for i in k:
+        if i ==0:
+            continue
+        f.write(nm[0]+"와"+nm[i]+"가 같은 파일인것 같습니다. MD5="+md[i]+"\n")
+    for i in k:
+        del(nm[i])
+        del(md[i])
+        t.write(" ".join(nm))
+        t.write(" ".join(md))
+t.write(" ".join(nm))
+t.write(" ".join(md))
+t.close()
+f.close()
