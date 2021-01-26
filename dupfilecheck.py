c1bcef789d7f7ba0ad5442ae819052b7c6e46a56
bbasung1 <bbasung1@daum.net>
2021-01-26 17:51:45 +0900

    test01

--- dupfilecheck.py
+++ dupfilecheck.py
@@ -6,7 +6,7 @@ for (path, dir, files) in os.walk("./"):
     for fn in files:
         if(os.path.isdir(os.path.join(path, fn))):
             continue
-        f = open(a, "rb")
+        f = open(os.path.join(path, fn), "rb")
         data = f.read()
         md.append(hashlib.md5(data).hexdigest())
-        mn.append(os.path.join(path, fn)
+        nm.append(os.path.join(path, fn))
