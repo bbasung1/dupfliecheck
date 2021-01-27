eb6c79fe6d6d7bab9d755252dedee43f58aabf59
bbasung01 <bbasung1@daum.net>
2021-01-28 03:41:04 +0900

    debug 3

--- dupfilecheck.py
+++ dupfilecheck.py
@@ -17,13 +17,15 @@ for a in md:
     for i, name in enumerate(md[k:]):
         t.write("\ncurrent a1:"+str(a)+"\n")
         t.write("\ncurrent i1:"+str(i)+"\n")
+        t.write("\ncurrent k1:"+str(k)+"\n")
+        t.write("\ncurrent r1:"+str(md.index(name))+"\n")
         if(a == name):
             t.write("\ncurrent a2:"+str(a)+"\n")
-            t.write("\ncurrent i2:"+str(a)+"\n")
-            f.write(nm[0]+"와"+nm[i]+"가 같은 파일인것 같습니다. MD5="+md[i]+"\n")
-            t.write("\ncurrent delposition:"+'\n'.join([str(i)]))
-            t.write("\ncheck nmdel:"+"\n".join(nm))
-            t.write("\ncheck mddel:"+"\n".join(md))
+            t.write("\ncurrent i2:"+str(i)+"\n")
+            f.write(nm[0]+"와"+nm[md.index(name)]+"가 같은 파일인것 같습니다. MD5="+md[md.index(name)]+"\n")
+            #t.write("\ncurrent delposition:"+'\n'.join([str(i+k)]))
+            #t.write("\ncheck nmdel:"+"\n".join(nm))
+            #t.write("\ncheck mddel:"+"\n".join(md))
         k+=1
 t.write("\nfinal nml:"+"\n".join(nm))
 t.write("\nfinal md:"+"\n".join(md))
