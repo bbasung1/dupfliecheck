01d5b3be292b02d9315a32f47b2a1f0da8a73f94
bbasung1 <bbasung1@daum.net>
2021-02-01 23:24:47 +0900

    before test

--- dupfilecheck.py
+++ dupfilecheck.py
@@ -10,14 +10,19 @@ for (path, dir, files) in os.walk("./"):
         f = open(os.path.join(path, fn), "rb")
         data = f.read()
         md5=(hashlib.md5(data).hexdigest())
+        print(os.path.join(path, fn))
+        print(md5)
         #nm.append(os.path.join(path, fn))
         co=0
         for i in md:
             if(i==md5):
+                print(i)
                 ind=md.index(i)
-                k.write(os.path.join(path, fn)+"와"+nm[ind]+"가 같은 파일인것 같습니다. md5="+i+"\n")
+                k.write(os.path.join(path, fn)+"와"+nm[ind]+"가 같은 파일인것 같습니다. md5="+md5+"\n")
                 co+=1
         if(co==0):
             nm.append(os.path.join(path, fn))
-            md.append(data)
+            md.append(md5)
+            print(nm)
+            print(md)
 k.close()
