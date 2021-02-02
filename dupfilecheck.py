001e3230bbe1845ea9ff9cef8b846c7660978d49
bbasung <bbasung1@daum.net>
2021-02-02 10:11:39 +0900

    use sqlite

--- dupfilecheck.py
+++ dupfilecheck.py
@@ -1,28 +1,31 @@
 import os
 import hashlib
-nm = []
-md = []
+import sqlite3
+if os.path.exists('temp.db'):
+    os.remove('temp.db')
+con=sqlite3.connect('temp.db')
+cur=con.cursor()
+cur.execute("create table temp(name text,md5 text)")
 k = open("log.txt", "w")
 for (path, dir, files) in os.walk("./"):
     for fn in files:
-        if(os.path.isdir(os.path.join(path, fn))):
+        pt=os.path.join(path, fn)
+        if(os.path.isdir(pt)):
             continue
-        f = open(os.path.join(path, fn), "rb")
+        f = open(pt, "rb")
         data = f.read()
         md5=(hashlib.md5(data).hexdigest())
-        print(os.path.join(path, fn))
+        print(pt)
         print(md5)
         #nm.append(os.path.join(path, fn))
-        co=0
-        for i in md:
-            if(i==md5):
-                print(i)
-                ind=md.index(i)
-                k.write(os.path.join(path, fn)+"와"+nm[ind]+"가 같은 파일인것 같습니다. md5="+md5+"\n")
-                co+=1
-        if(co==0):
-            nm.append(os.path.join(path, fn))
-            md.append(md5)
-            print(nm)
-            print(md)
+        try:
+            cur.execute("select * from temp where md5="+md5)
+            row=cur.fetchone()
+            data1=row[0]
+            data2=row[1]
+            f.write(pt+"와"+data1+"가 같은 파일인거 같습니다. md5="+data2+"\n")
+        except:
+            cur.execute("insert into temp values('"+pt+"','"+md5+"')")
+            con.commit()
 k.close()
+con.close()
